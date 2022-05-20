9f226684362a08dde74bf47e41a3de5bfe0b9e3a
Shrey Suri <shreysuri5@gmail.com>
2022-05-19 21:41:34 -0500

    starting to code

---
+++ MessageEncoder_1.py
@@ -0,0 +1,32 @@
+import random
+import math
+
+def factor(x):
+    a = int(math.sqrt(x)) + 1
+    factors = 0
+    for i in range (1, a):
+        if (x/i) % 1 == 0:
+            factors = factors + 2
+        else:
+            factors = factors + 0
+    if math.sqrt(x) % 1 == 0:
+        factors = factors - 1
+    else:
+        factors = factors - 0
+    return(factors)
+
+
+user_given_prime = int(input(print("If you would like to encode with a certain prime, plese enter it now. If not, please enter '0'. ")))
+
+if user_given_prime % 1 == 0 and user_given_prime > 0:
+    # prime check
+elif user_given_prime == 0:
+    user_given_prime = False
+    prime = None
+else:
+    print("Please reload the program and type either a prime number or '0' ")
+
+
+if user_given_prime == False:
+    while prime == None:
+        
