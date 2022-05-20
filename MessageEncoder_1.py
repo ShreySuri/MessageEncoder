fc38cfe23c44d02f47a6a44989af05719666a4b1
Shrey Suri <shreysuri5@gmail.com>
2022-05-19 21:53:20 -0500

    added list of primes 100 - 999

--- MessageEncoder_1.py
+++ MessageEncoder_1.py
@@ -15,11 +15,17 @@ def factor(x):
         factors = factors - 0
     return(factors)
 
+primes_list = [101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+
+
 
 user_given_prime = int(input(print("If you would like to encode with a certain prime, plese enter it now. If not, please enter '0'. ")))
 
 if user_given_prime % 1 == 0 and user_given_prime > 0:
-    # prime check
+    if factor(user_given_prime) == 2:
+        prime = user_given_prime
+    else:
+        print("Please reload the program and type either a prime number or '0' ") 
 elif user_given_prime == 0:
     user_given_prime = False
     prime = None
@@ -30,4 +36,3 @@ else:
 if user_given_prime == False:
     while prime == None:
         
-
