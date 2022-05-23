b0b9b5203cc9f653ca4a89304f659b7078fa7116
Shrey Suri <shreysuri5@gmail.com>
2022-05-22 21:32:41 -0500

    done with boilerplate stuff

--- MessageEncoder_1.py
+++ MessageEncoder_1.py
@@ -15,29 +15,40 @@ def factor(x):
         factors = factors - 0
     return(factors)
 
-primes_list = [101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
-primes_count = 143
+prime = None
 
-
-user_given_prime = int(input(print("If you would like to encode with a certain prime, plese enter it now. If not, please enter '0'. ")))
-
-if user_given_prime % 1 == 0 and user_given_prime > 0:
-    if factor(user_given_prime) == 2:
-        prime = user_given_prime
+while prime == None:
+    prime = int(input(print("Please choose a prime number. ")))
+    if factor(prime) == 2:
+        prime = prime
     else:
-        print("Please reload the program and type either a prime number or '0' ")
         prime = None
-elif user_given_prime == 0:
-    prime = None
-else:
-    print("Please reload the program and type either a prime number or '0' ")
 
-if prime == None:
-    prime_picker = random.randint(0,142)
-    prime = primes_list[prime_picker]
+a = "01"
+b = "02"
+c = "03"
+d = "04"
+e = "05"
+f = "06"
+g = "07"
+h = "08"
+i = "09"
+j = "10"
+k = "11"
+l = "12"
+m = "13"
+n = "14"
+o = "15"
+p = "16"
+q = "17"
+r = "18"
+s = "19"
+t = "20"
+u = "21"
+v = "22"
+w = "23"
+x = "24"
+y = "25"
+x = "26"
 
-
-primes.remove(prime)
-
-letter_a = random.randint(0,141)
-
+code
